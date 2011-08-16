41a087ad89c48624e7417ab279a1c1b007137410
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-08-16 20:39:30 +0100

    configure log4j

--- src/test/groovy/betamax/util/EchoServer.groovy
+++ src/test/groovy/betamax/util/EchoServer.groovy
@@ -1,5 +1,9 @@
 package betamax.util
 
+import groovy.util.logging.Log4j
+import java.util.concurrent.CountDownLatch
+
+@Log4j
 class EchoServer {
 
     private Thread t
@@ -7,11 +11,15 @@ class EchoServer {
     String start() {
         def host = InetAddress.localHost.hostAddress
         def port = 5000
+
+		def readyLatch = new CountDownLatch(1)
+
         def serverProcess =  {
             def server = new ServerSocket(port)
             try {
-                server.accept() { socket ->
-                    println "got request"
+				readyLatch.countDown()
+                server.accept { socket ->
+                    log.debug "got request..."
                     socket.withStreams { input, output ->
                         output.withWriter { writer ->
                             writer << "HTTP/1.1 200 OK\n"
@@ -23,14 +31,16 @@ class EchoServer {
                     }
                 }
             } catch(InterruptedException e) {
-                println "interrupted"
+                log.info "interrupted"
             } finally {
                 server.close()
             }
         }
+		
         t = new Thread(serverProcess)
         t.setPriority(Thread.MIN_PRIORITY)
         t.start()
+		readyLatch.await()
 
         "http://$host:$port/"
     }
