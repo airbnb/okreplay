d299ca3a2d6e463c0d94ee4989ee01f2cf3f6453
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-08-22 16:52:56 +0100

    updated readme

--- readme.md
+++ readme.md
@@ -1,22 +1,94 @@
 # Betamax
 
-A Groovy testing tool inspired by Ruby's [VCR][1].
+A Groovy testing tool inspired by Ruby's [VCR][1]. Betamax can record and play back HTTP interactions made by your app
+so that your tests can run without any real HTTP traffic going to external URLs. The first time a test is run any HTTP
+traffic is recorded to a _tape_ and subsequent runs will play back the HTTP response without really connecting to the
+external endpoint.
 
-This project is currently in the prototype stage.
+Tapes are stored to disk as JSON and can be modified (or even created) by hand. Different tests can use different tapes
+to simulate varying responses from external endpoints.
+
+## Usage
+
+To use Betamax you just need to annotate your JUnit test or [Spock][2] specifications with `@Betamax(tape="tape_name")`
+and include a `betamax.Recorder` Rule.
+
+### JUnit example
+
+    import betamax.Betamax
+    import betamax.Recorder
+    import org.junit.*
+
+    class MyTest {
+
+	        @Rule public Recorder recorder = Recorder.instance
+
+            @Betamax(tape="my_tape")
+            @Test
+            void testMethodThatAccessesExternalWebService() {
+
+            }
+
+    }
+
+### Spock example
+
+    import betamax.Betamax
+    import betamax.Recorder
+    import org.junit.*
+    import spock.lang.*
+
+    class MySpec extends Specification {
+
+	        @Rule Recorder recorder = Recorder.instance
+
+            @Betamax(tape="my_tape")
+            def "test method that accesses external web service"() {
+
+            }
+
+    }
+
+## Caveats
+
+By default [Apache _HTTPClient_][3] takes no notice of Java's HTTP proxy settings. If you are using HTTPClient then the
+Betamax proxy can only intercept HTTP traffic if the client is set up to use a `[ProxySelectorRoutePlanner][5]`.
+
+### Configuring HTTPClient
+
+    def client = new DefaultHttpClient()
+    client.routePlanner = new ProxySelectorRoutePlanner(client.connectionManager.schemeRegistry, ProxySelector.default)
+
+The same is true of [Groovy _HTTPBuilder_][4] and its [_RESTClient_][6] variant as they are wrappers around
+_HTTPClient_.
+
+### Configuring HTTPBuilder
+
+    def http = new HTTPBuilder("http://groovy.codehaus.org")
+    def routePlanner = new ProxySelectorRoutePlanner(http.client.connectionManager.schemeRegistry, ProxySelector.default)
+    http.client.routePlanner = routePlanner
+
+_HTTPBuilder_ also includes a [_HttpURLClient_][7] class which needs no special configuration as it uses a
+`java.net.URLConnection` rather than _HTTPClient_.
 
 ## Roadmap
 
-* `Betamax.withTape(name, Closure)` as a way of scoping proxy usage
-* `@Betamax(tape=name)` for tests/specs
+* Configure Recorder via annotation
+* Test with HTTPClient 3.x
 * Description in interactions
 * Non-text responses
 * Multipart requests
-* Calculate content length header rather than specifying in tape
 * Rotate multiple responses for same URL on same tape
 * Throw exceptions if tape not inserted & proxy gets hit
-* Allow groovy in tape files
+* Allow groovy evaluation in tape files
 * Match requests based on URI, host, path, method, body, headers
 * Optionally re-record tapes after TTL expires
 * Record modes
 
 [1]:https://github.com/myronmarston/vcr
+[2]:http://spockframework.org/
+[3]:http://hc.apache.org/httpcomponents-client-ga/httpclient/index.html
+[4]:http://groovy.codehaus.org/modules/http-builder/
+[5]:http://hc.apache.org/httpcomponents-client-ga/httpclient/apidocs/org/apache/http/impl/conn/ProxySelectorRoutePlanner.html
+[6]:http://groovy.codehaus.org/modules/http-builder/doc/rest.html
+[7]:http://groovy.codehaus.org/modules/http-builder/doc/httpurlclient.html
--- src/main/groovy/betamax/server/HttpProxyServer.groovy
+++ src/main/groovy/betamax/server/HttpProxyServer.groovy
@@ -5,85 +5,81 @@ import groovy.util.logging.Log4j
 import org.apache.http.impl.nio.DefaultServerIOEventDispatch
 import org.apache.http.impl.nio.reactor.DefaultListeningIOReactor
 import org.apache.http.nio.NHttpConnection
-import org.apache.http.nio.reactor.IOReactor
+import org.apache.http.nio.protocol.BufferingHttpServiceHandler
 import org.apache.http.params.SyncBasicHttpParams
 import org.apache.http.*
 import org.apache.http.impl.*
-import org.apache.http.nio.protocol.*
+import org.apache.http.nio.reactor.*
 import static org.apache.http.params.CoreConnectionPNames.*
 import static org.apache.http.params.CoreProtocolPNames.ORIGIN_SERVER
 import org.apache.http.protocol.*
-import org.apache.http.nio.reactor.IOReactorStatus
 
 /**
- * Basic, yet fully functional and spec compliant, HTTP/1.1 server based on the non-blocking 
- * I/O model.
- * <p>
- * Please note the purpose of this application is demonstrate the usage of HttpCore APIs.
- * It is NOT intended to demonstrate the most efficient way of building an HTTP server. 
+ * A simple proxy server that can run in the background. The code here is based on the "Basic non-blocking HTTP server"
+ * example from http://hc.apache.org/httpcomponents-core-ga/examples.html
  */
 @Log4j
-class HttpProxyServer implements EventListener {
+class HttpProxyServer implements org.apache.http.nio.protocol.EventListener {
 
-	private IOReactor reactor
-	final int port
+    private IOReactor reactor
+    final int port
 
     private String originalProxyHost
     private String originalProxyPort
 
     HttpProxyServer() {
-		port = 5555
-	}
+        port = 5555
+    }
 
-	void start(Recorder recorder) {
-		def params = new SyncBasicHttpParams()
-		params.setIntParameter(SO_TIMEOUT, 5000)
-		params.setIntParameter(SOCKET_BUFFER_SIZE, 8 * 1024)
-		params.setBooleanParameter(STALE_CONNECTION_CHECK, false)
-		params.setBooleanParameter(TCP_NODELAY, true)
-		params.setParameter(ORIGIN_SERVER, "HttpComponents/1.1")
+    void start(Recorder recorder) {
+        def params = new SyncBasicHttpParams()
+        params.setIntParameter(SO_TIMEOUT, 5000)
+        params.setIntParameter(SOCKET_BUFFER_SIZE, 8 * 1024)
+        params.setBooleanParameter(STALE_CONNECTION_CHECK, false)
+        params.setBooleanParameter(TCP_NODELAY, true)
+        params.setParameter(ORIGIN_SERVER, "HttpComponents/1.1")
 
-		def httpproc = new ImmutableHttpProcessor([
-				new ResponseDate(),
-				new ResponseServer(),
-				new ResponseContent(),
-				new ResponseConnControl()
-		] as HttpResponseInterceptor[])
+        def httpproc = new ImmutableHttpProcessor([
+                new ResponseDate(),
+                new ResponseServer(),
+                new ResponseContent(),
+                new ResponseConnControl()
+        ] as HttpResponseInterceptor[])
 
-		def handler = new BufferingHttpServiceHandler(
-				httpproc,
-				new DefaultHttpResponseFactory(),
-				new DefaultConnectionReuseStrategy(),
-				params)
+        def handler = new BufferingHttpServiceHandler(
+                httpproc,
+                new DefaultHttpResponseFactory(),
+                new DefaultConnectionReuseStrategy(),
+                params)
 
-		def reqistry = new HttpRequestHandlerRegistry()
-		reqistry.register "*", new HttpProxyHandler(recorder)
+        def reqistry = new HttpRequestHandlerRegistry()
+        reqistry.register "*", new HttpProxyHandler(recorder)
 
-		handler.handlerResolver = reqistry
-		handler.eventListener = this
+        handler.handlerResolver = reqistry
+        handler.eventListener = this
 
-		def ioEventDispatch = new DefaultServerIOEventDispatch(handler, params)
-		reactor = new DefaultListeningIOReactor(2, params)
-		reactor.listen(new InetSocketAddress(port))
+        def ioEventDispatch = new DefaultServerIOEventDispatch(handler, params)
+        reactor = new DefaultListeningIOReactor(2, params)
+        reactor.listen(new InetSocketAddress(port))
 
         overrideProxySettings()
 
-		Thread.start {
-			log.debug "starting proxy server..."
-			reactor.execute(ioEventDispatch)
-		}
+        Thread.start {
+            log.debug "starting proxy server..."
+            reactor.execute(ioEventDispatch)
+        }
 
-		while (reactor.status != IOReactorStatus.ACTIVE) {
-			log.debug "waiting..."
-			sleep 100
-		}
-	}
+        while (reactor.status != IOReactorStatus.ACTIVE) {
+            log.debug "waiting..."
+            sleep 100
+        }
+    }
 
-	void stop() {
-		log.debug "stopping proxy server..."
+    void stop() {
+        log.debug "stopping proxy server..."
         restoreOriginalProxySettings()
-		reactor.shutdown()
-	}
+        reactor.shutdown()
+    }
 
     private void overrideProxySettings() {
         originalProxyHost = System.properties."http.proxyHost"
@@ -105,25 +101,25 @@ class HttpProxyServer implements EventListener {
         }
     }
 
-	void connectionOpen(final NHttpConnection conn) {
-		log.info "Connection open: $conn"
-	}
+    void connectionOpen(final NHttpConnection conn) {
+        log.info "Connection open: $conn"
+    }
 
-	void connectionTimeout(final NHttpConnection conn) {
-		log.info "Connection timed out: $conn"
-	}
+    void connectionTimeout(final NHttpConnection conn) {
+        log.info "Connection timed out: $conn"
+    }
 
-	void connectionClosed(final NHttpConnection conn) {
-		log.info "Connection closed: $conn"
-	}
+    void connectionClosed(final NHttpConnection conn) {
+        log.info "Connection closed: $conn"
+    }
 
-	void fatalIOException(final IOException ex, final NHttpConnection conn) {
-		log.error "I/O error: $ex.message"
-	}
+    void fatalIOException(final IOException ex, final NHttpConnection conn) {
+        log.error "I/O error: $ex.message"
+    }
 
-	void fatalProtocolException(final HttpException ex, final NHttpConnection conn) {
-		log.error "HTTP error: $ex.message"
-	}
+    void fatalProtocolException(final HttpException ex, final NHttpConnection conn) {
+        log.error "HTTP error: $ex.message"
+    }
 
 }
 
