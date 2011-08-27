0c8eeed7c0c18c66529fb441bf3eb39818dbca5a
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-08-28 00:31:29 +0100

    Extended test coverage for #21

--- src/test/groovy/betamax/ContentCharsetSpec.groovy
+++ src/test/groovy/betamax/ContentCharsetSpec.groovy
@@ -13,8 +13,8 @@ import spock.lang.*
 @Issue("https://github.com/robfletcher/betamax/issues/21")
 class ContentCharsetSpec extends Specification {
 
-	@Unroll({"a response with a $charset body is handled correctly"})
-	def "response body's charset is handled correctly"() {
+	@Unroll({"a response with a $charset body is recorded correctly"})
+	def "response body's charset is recorded correctly"() {
 		given:
 		def request = new HttpGet("http://icanhascheezburger.com/")
 
@@ -38,6 +38,48 @@ class ContentCharsetSpec extends Specification {
 		then:
 		def yaml = writer.toString()
 		yaml.contains("body: \u00a3")
+
+		where:
+		charset      | encoding  | encoder
+		"UTF-8"      | "none"    | null
+		"ISO-8859-1" | "none"    | null
+		"UTF-8"      | "gzip"    | new GzipEncoder()
+		"ISO-8859-1" | "gzip"    | new GzipEncoder()
+		"UTF-8"      | "deflate" | new DeflateEncoder()
+		"ISO-8859-1" | "deflate" | new DeflateEncoder()
+	}
+
+	@Unroll({"a response with a $charset body is played back correctly"})
+	def "response body's charset is played back correctly"() {
+		given:
+		def yaml = """\
+!tape
+name: charsets
+interactions:
+- recorded: 2011-08-27T23:25:45.000Z
+  request:
+    protocol: HTTP/1.1
+    method: GET
+    uri: http://icanhascheezburger.com/
+  response:
+    protocol: HTTP/1.1
+    status: 200
+    headers:
+      Content-Type: text/plain;charset=$charset
+      Content-Encoding: $encoding
+    body: \u00a3
+"""
+		def tape = YamlTape.readFrom(new StringReader(yaml))
+
+		and:
+		def response = new BasicHttpResponse(HTTP_1_1, HTTP_OK, "OK")
+
+		when:
+		tape.seek(new HttpGet("http://icanhascheezburger.com/"))
+		tape.play(response)
+
+		then:
+		response.entity.content.bytes == encoder ? encoder.encode("\u00a3", charset) : "\u00a3".getBytes(charset)
 
 		where:
 		charset      | encoding  | encoder
