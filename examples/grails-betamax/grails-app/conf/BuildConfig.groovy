1217a3210e026b0ff4aa84da18dc67b3004d7421
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-10-12 22:43:21 +0100

    #39: force use of slf4j 1.6.2

--- examples/grails-betamax/grails-app/conf/BuildConfig.groovy
+++ examples/grails-betamax/grails-app/conf/BuildConfig.groovy
@@ -3,7 +3,9 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.dependency.resolution = {
-	inherits "global"
+	inherits("global") {
+		excludes "slf4j-api", "slf4j-log4j12", "jul-to-slf4j", "jcl-over-slf4j"
+	}
 	log "warn"
 	repositories {
 		grailsPlugins()
@@ -14,13 +16,15 @@ grails.project.dependency.resolution = {
 		mavenRepo "http://oss.sonatype.org/content/groups/public/"
 	}
 	dependencies {
+		// workaround for https://github.com/robfletcher/betamax/issues/39
+		def slf4jVersion = "1.6.2"
+		compile "org.slf4j:slf4j-api:$slf4jVersion"
+		runtime "org.slf4j:slf4j-log4j12:$slf4jVersion", "org.slf4j:jul-to-slf4j:$slf4jVersion", "org.slf4j:jcl-over-slf4j:$slf4jVersion"
+
 		compile("org.codehaus.groovy.modules.http-builder:http-builder:0.5.1") {
 			excludes "groovy", "xml-apis"
 		}
-		test("com.github.robfletcher:betamax:1.1-SNAPSHOT") {
-			excludes "jetty-server"
-		}
-		test "org.eclipse.jetty:jetty-server:7.3.1.v20110307"
+		test "com.github.robfletcher:betamax:1.1-SNAPSHOT"
 	}
 	plugins {
 		compile ":hibernate:$grailsVersion"
