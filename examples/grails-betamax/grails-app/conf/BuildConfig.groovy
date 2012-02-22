73e4b00ccb91d5fe51a113d12056366335bef7e8
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-02-22 13:26:28 +0000

    use local target dir in example project

--- examples/grails-betamax/grails-app/conf/BuildConfig.groovy
+++ examples/grails-betamax/grails-app/conf/BuildConfig.groovy
@@ -1,3 +1,4 @@
+grails.project.work.dir = "target"
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
