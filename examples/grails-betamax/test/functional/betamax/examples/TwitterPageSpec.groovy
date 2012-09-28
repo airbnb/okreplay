f216161d64f7c5af4ef12c89b926f47353b6ae9d
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-09-28 17:14:53 +0100

    updated grails tests

--- examples/grails-betamax/test/functional/betamax/examples/TwitterPageSpec.groovy
+++ examples/grails-betamax/test/functional/betamax/examples/TwitterPageSpec.groovy
@@ -12,47 +12,47 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver
 
 class TwitterPageSpec extends GebSpec {
 
-	File baseDir = BuildSettingsHolder.settings?.baseDir ?: new File("examples/grails-betamax")
-	@Rule Recorder recorder = new Recorder(tapeRoot: new File(baseDir, "test/resources/tapes"), ignoreLocalhost: true)
+	File baseDir = BuildSettingsHolder.settings?.baseDir ?: new File('examples/grails-betamax')
+	@Rule Recorder recorder = new Recorder(tapeRoot: new File(baseDir, 'test/resources/tapes'), ignoreLocalhost: true)
 
-	def setupSpec() {
+	void setupSpec() {
 		def restClient = ApplicationHolder.application.mainContext.restClient
 		BetamaxRoutePlanner.configure(restClient.client)
 	}
 
-	def setup() {
-		browser.baseUrl = "http://localhost:8080/grails-betamax/"
+	void setup() {
+		browser.baseUrl = 'http://localhost:8080/grails-betamax/'
 		HtmlUnitDriver driver = browser.driver
-		def proxyConfig = new ProxyConfig("localhost", 5555)
-		proxyConfig.addHostsToProxyBypass("localhost")
+		def proxyConfig = new ProxyConfig('localhost', 5555)
+		proxyConfig.addHostsToProxyBypass('localhost')
 		driver.webClient.proxyConfig = proxyConfig
 		//driver.setJavascriptEnabled(true) //not work. see http://groups.google.com/group/webdriver/browse_thread/thread/6bbb18dca79c4e92?pli=1
 	}
 
-	@Betamax(tape = "twitter success")
-	def "displays list of tweets based on query"() {
+	@Betamax(tape = 'twitter success')
+	void 'displays list of tweets based on query'() {
 		given:
-		go "twitter"
+		go 'twitter'
 
 		expect:
-		title == "Twitter Search Results"
+		title == 'Twitter Search Results'
 
 		and:
 		$('#tweets li').size() == 10
 		$('#tweets li p')*.text().every { it =~ /(?i)#gr8conf/ }
-		$('#tweets li').eq(0).find('p').text() == "Slides for my #gr8conf talk: \u2018Spock Soup to Nuts\u2019. Thanks to everyone who showed up! http://t.co/CNA9ertp"
+		$('#tweets li').eq(0).find('p').text() == 'Slides for my #gr8conf talk: \u2018Spock Soup to Nuts\u2019. Thanks to everyone who showed up! http://t.co/CNA9ertp'
 	}
 
-	@Betamax(tape = "twitter success")
-	def "can follow a link to a twitter user"() {
+	@Betamax(tape = 'twitter success')
+	void 'can follow a link to a twitter user'() {
 		given:
-		go "twitter"
+		go 'twitter'
 
 		when:
 		$('#tweets li').eq(0).find('small a').click()
 
 		then:
-		$('.user-info .username').text() == "@zanthrash"
+		$('.user-info .username').text() == '@zanthrash'
 	}
 
 }
