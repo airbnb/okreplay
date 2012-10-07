49a89d9adf624ee8a3e65997ae1ca78dc7c882bf
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-10-07 06:28:54 +0100

    guarantee test execution order in `AnnotationTest`
    Fixes #67

--- src/test/groovy/co/freeside/betamax/AnnotationTest.groovy
+++ src/test/groovy/co/freeside/betamax/AnnotationTest.groovy
@@ -5,13 +5,18 @@ import co.freeside.betamax.util.httpbuilder.BetamaxRESTClient
 import co.freeside.betamax.util.server.EchoHandler
 import groovyx.net.http.RESTClient
 import org.junit.*
+import org.junit.runner.RunWith
+import org.junit.runners.BlockJUnit4ClassRunner
+import org.junit.runners.model.FrameworkMethod
 import static co.freeside.betamax.proxy.jetty.BetamaxProxy.X_BETAMAX
+import static co.freeside.betamax.util.FileUtils.newTempDir
 import static java.net.HttpURLConnection.HTTP_OK
 import static org.apache.http.HttpHeaders.VIA
 
+@RunWith(OrderedRunner)
 class AnnotationTest {
 
-	static File tapeRoot = new File(System.properties.'java.io.tmpdir', 'tapes')
+	static File tapeRoot = newTempDir('tapes')
 	@Rule public Recorder recorder = new Recorder(tapeRoot: tapeRoot)
 	SimpleServer endpoint = new SimpleServer()
 	RESTClient http
@@ -79,4 +84,32 @@ class AnnotationTest {
 		assert response.getFirstHeader(VIA) == null
 	}
 
+}
+
+/**
+ * This is an evil hack. JUnit does not guarantee test execution order and the methods in this test depend on each
+ * other. In particular `annotatedTestCanPlayBack` will fail if run before `annotatedTestCanRecord`. Really the tests
+ * should be idempotent.
+ */
+class OrderedRunner extends BlockJUnit4ClassRunner {
+
+	private static final ORDER = [
+			'noTapeIsInsertedIfThereIsNoAnnotationOnTheTest',
+			'annotationOnTestCausesTapeToBeInserted',
+			'tapeIsEjectedAfterAnnotatedTestCompletes',
+			'annotatedTestCanRecord',
+			'annotatedTestCanPlayBack',
+			'canMakeUnproxiedRequestAfterUsingAnnotation'
+	]
+
+	OrderedRunner(Class testClass) {
+		super(testClass)
+	}
+
+	@Override
+	protected List<FrameworkMethod> computeTestMethods() {
+		super.computeTestMethods().sort {FrameworkMethod o1, FrameworkMethod o2 ->
+			ORDER.indexOf(o1.name) <=> ORDER.indexOf(o2.name)
+		}
+	}
 }
