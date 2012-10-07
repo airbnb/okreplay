4f1a2a3e8f54a2f384afdad310a76516e4ef0013
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-10-07 05:19:30 +0100

    Test for HTTP basic auth support
    Fixes #48

--- src/test/groovy/co/freeside/betamax/BasicAuthSpec.groovy
+++ src/test/groovy/co/freeside/betamax/BasicAuthSpec.groovy
@@ -1,31 +1,69 @@
 package co.freeside.betamax
 
 import co.freeside.betamax.util.httpbuilder.BetamaxRESTClient
-import groovyx.net.http.RESTClient
+import org.apache.http.HttpHost
 import org.apache.http.auth.*
 import org.junit.Rule
 import spock.lang.*
-import static org.apache.http.auth.AuthScope.ANY
+import static co.freeside.betamax.MatchRule.*
+import static co.freeside.betamax.TapeMode.*
+import static co.freeside.betamax.proxy.jetty.BetamaxProxy.X_BETAMAX
+import static co.freeside.betamax.util.FileUtils.newTempDir
+import static java.net.HttpURLConnection.*
 
+@Unroll
+@Stepwise
 class BasicAuthSpec extends Specification {
 
-	static final URL endpoint = 'http://httpbin.org/basic-auth/user/passwd'.toURL()
+	@Shared endpoint = 'http://httpbin.org/basic-auth/user/passwd'.toURL()
+	@Shared targetHost = new HttpHost(endpoint.host, endpoint.port)
 
-	@Shared @AutoCleanup('deleteDir') File tapeRoot = new File(System.properties.'java.io.tmpdir', 'tapes')
+	@Shared @AutoCleanup('deleteDir') File tapeRoot = newTempDir('tapes')
 	@Rule Recorder recorder = new Recorder(tapeRoot: tapeRoot)
-	RESTClient http = new BetamaxRESTClient()
+	def http = new BetamaxRESTClient()
 
-	@Betamax(tape = 'basic auth')
-	void 'can record response from authenticated endpoint'() {
+	void setup() {
+		http.handler[HTTP_UNAUTHORIZED] = { resp -> resp }
+	}
+
+	@Betamax(tape = 'basic auth', mode = WRITE_ONLY, match = [method, uri, headers])
+	void 'can record #status response from authenticated endpoint'() {
 		given:
-		http.client.credentialsProvider.setCredentials(ANY, new UsernamePasswordCredentials('user', 'passwd'))
+		http.client.credentialsProvider.setCredentials(new AuthScope(targetHost), credentials)
 
 		when:
 		def response = http.get(uri: endpoint)
 
 		then:
-		response.data.authenticated
-		response.data.user == 'user'
+		response.status == status
+		response.getFirstHeader(X_BETAMAX).value == 'REC'
+
+		where:
+		password    | status
+		'passwd'    | HTTP_OK
+		'INCORRECT' | HTTP_UNAUTHORIZED
+
+		credentials = new UsernamePasswordCredentials('user', password)
+	}
+
+	@Betamax(tape = 'basic auth', mode = READ_ONLY, match = [method, uri, headers])
+	void 'can play back #status response from authenticated endpoint'() {
+		given:
+		http.client.credentialsProvider.setCredentials(new AuthScope(targetHost), credentials)
+
+		when:
+		def response = http.get(uri: endpoint)
+
+		then:
+		response.status == status
+		response.getFirstHeader(X_BETAMAX).value == 'PLAY'
+
+		where:
+		password    | status
+		'passwd'    | HTTP_OK
+		'INCORRECT' | HTTP_UNAUTHORIZED
+
+		credentials = new UsernamePasswordCredentials('user', password)
 	}
 
 }
