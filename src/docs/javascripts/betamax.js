79d96a8efb2369376df83236b7af06618c915d48
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-10-11 16:53:40 +0100

    oops, left some flattr stuff in docs

--- src/docs/javascripts/betamax.js
+++ src/docs/javascripts/betamax.js
@@ -39,12 +39,6 @@ $(document).ready(function() {
 			.find('li:first-child a').tab('show');
 	});
 
-    var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
-    s.type = 'text/javascript';
-    s.async = true;
-    s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
-    t.parentNode.insertBefore(s, t);
-
 	// FOUC prevention
 	$(window).load(function() {
 		// affix nav
