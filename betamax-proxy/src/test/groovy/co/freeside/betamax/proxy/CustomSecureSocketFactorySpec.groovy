e12a2aaabe632aab66e872aebda2dfaeb8b9db28
robfletcher <robert.w.fletcher@gmail.com>
2013-01-04 15:36:14 +0000

    #72 fixed spec & proved functionality works

--- betamax-proxy/src/test/groovy/co/freeside/betamax/proxy/CustomSecureSocketFactorySpec.groovy
+++ betamax-proxy/src/test/groovy/co/freeside/betamax/proxy/CustomSecureSocketFactorySpec.groovy
@@ -1,16 +1,14 @@
 package co.freeside.betamax.proxy
 
 import java.security.KeyStore
-import co.freeside.betamax.*
+import co.freeside.betamax.ProxyRecorder
 import co.freeside.betamax.httpclient.BetamaxHttpsSupport
 import co.freeside.betamax.proxy.jetty.SimpleServer
 import co.freeside.betamax.proxy.ssl.DummySSLSocketFactory
 import co.freeside.betamax.util.server.*
 import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.HttpGet
-import org.apache.http.conn.ssl.SSLSocketFactory
 import org.apache.http.impl.client.SystemDefaultHttpClient
-import org.junit.Rule
 import spock.lang.*
 import static co.freeside.betamax.util.FileUtils.newTempDir
 import static co.freeside.betamax.util.server.HelloHandler.HELLO_WORLD
@@ -22,9 +20,7 @@ class CustomSecureSocketFactorySpec extends Specification {
 
 	@Shared @AutoCleanup('deleteDir') File tapeRoot = newTempDir('tapes')
 	@Shared @AutoCleanup('stop') SimpleServer httpsEndpoint = new SimpleSecureServer(5001)
-	@Shared SSLSocketFactory sslSocketFactory
-
-	@Rule @AutoCleanup('ejectTape') ProxyRecorder recorder = new ProxyRecorder(tapeRoot: tapeRoot, sslSupport: true, sslSocketFactory: sslSocketFactory)
+	@Shared KeyStore trustStore
 
 	@Shared URI httpsUri
 
@@ -35,10 +31,8 @@ class CustomSecureSocketFactorySpec extends Specification {
 
 		httpsUri = httpsEndpoint.url.toURI()
 
-		def trustStore = KeyStore.getInstance(KeyStore.defaultType)
+		trustStore = KeyStore.getInstance(KeyStore.defaultType)
 		trustStore.load(null, null)
-
-		sslSocketFactory = Spy(DummySSLSocketFactory, constructorArgs: [trustStore])
 	}
 
 	void setup() {
@@ -46,22 +40,29 @@ class CustomSecureSocketFactorySpec extends Specification {
 		BetamaxHttpsSupport.configure(http)
 	}
 
-	@Betamax(tape = 'custom secure socket factory spec', mode = TapeMode.WRITE_ONLY)
 	void 'proxy can use a custom SSL socket factory'() {
+
+		given: 'a recorder configured with a custom SSL socket factory'
+		def sslSocketFactory = Spy(DummySSLSocketFactory, constructorArgs: [trustStore])
+		def recorder = new ProxyRecorder(tapeRoot: tapeRoot, sslSupport: true, sslSocketFactory: sslSocketFactory)
+		recorder.start 'custom secure socket factory spec'
 
 		when: 'an HTTPS request is made'
 		def request = new HttpGet(httpsEndpoint.url)
 		def response = http.execute(request)
 
-		and: 'it is intercepted by the proxy'
-		assert response.statusLine.statusCode == SC_OK
-		assert response.getFirstHeader(VIA)?.value == 'Betamax'
+		then: 'it is intercepted by the proxy'
+		response.statusLine.statusCode == SC_OK
+		response.getFirstHeader(VIA)?.value == 'Betamax'
 
 		and: 'the response body is readable'
-		assert response.entity.content.text == HELLO_WORLD
+		response.entity.content.text == HELLO_WORLD
 
-		then: 'the custom ssl socket factory was used'
+		and: 'the custom ssl socket factory was used'
 		(1.._) * sslSocketFactory._
+
+		cleanup:
+		recorder.stop()
 
 		where:
 		url << [httpsEndpoint.url]
