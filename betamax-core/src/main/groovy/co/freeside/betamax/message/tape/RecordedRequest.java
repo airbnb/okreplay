cdd72bf09a32eb53c015e9f68b63f47e47c77c69
Sean Freitag <freitag.sean@gmail.com>
2013-08-08 18:54:40 -0500

    Converting RecordedRequest to Java.

---
+++ betamax-core/src/main/groovy/co/freeside/betamax/message/tape/RecordedRequest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2011 Rob Fletcher
+ *
+ * Converted from Groovy to Java by Sean Freitag
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.freeside.betamax.message.tape;
+
+import co.freeside.betamax.message.Request;
+
+import java.net.URI;
+
+public class RecordedRequest extends RecordedMessage implements Request {
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public URI getUri() {
+        return uri;
+    }
+
+    public void setUri(URI uri) {
+        this.uri = uri;
+    }
+
+    private String method;
+    private URI uri;
+}
