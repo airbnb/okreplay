1d6071f460160777bc65af365169908b6160e853
Sean Freitag <freitag.sean@gmail.com>
2013-08-08 16:57:34 -0500

    Converting BasicRequest to Java.

---
+++ betamax-core/src/main/groovy/co/freeside/betamax/util/message/BasicRequest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2011 Rob Fletcher
+ *
+ * Converted from Groovy to Java by Sean Freitag
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.freeside.betamax.util.message;
+
+import co.freeside.betamax.message.Request;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class BasicRequest extends BasicMessage implements Request {
+    public BasicRequest() { }
+
+    public BasicRequest(String method, String uri) {
+        try {
+            this.method = method;
+            this.uri = new URI(uri);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public URI getUri() {
+        return uri;
+    }
+
+    public void setUri(URI uri) {
+        this.uri = uri;
+    }
+
+    private String method;
+    private URI uri;
+}
