6494150061e123aa16abe895a3307f1a5c890c5d
Sean Freitag <freitag.sean@gmail.com>
2013-08-09 08:07:38 -0500

    Converting NoTapeException to Java.

---
+++ betamax-core/src/main/groovy/co/freeside/betamax/handler/NoTapeException.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2011 Rob Fletcher
+ *
+ * Converted from Groovy to Java by Sean Freitag
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.freeside.betamax.handler;
+
+import static java.net.HttpURLConnection.HTTP_FORBIDDEN;
+
+public class NoTapeException extends HandlerException {
+    public NoTapeException() {
+        super("No tape");
+    }
+
+    @Override
+    public int getHttpStatus() {
+        return HTTP_FORBIDDEN;
+    }
+
+}
