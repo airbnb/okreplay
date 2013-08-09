da9ae9173f4fb57e0e8aeb2637a5c89226609513
Sean Freitag <freitag.sean@gmail.com>
2013-08-09 08:11:35 -0500

    Converting TargetTimeoutException to Java.

---
+++ betamax-core/src/main/groovy/co/freeside/betamax/handler/TargetTimeoutException.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2011 Rob Fletcher
+ *
+ * Converted from Groovy to Java by Sean Freitag
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.freeside.betamax.handler;
+
+import static java.net.HttpURLConnection.HTTP_GATEWAY_TIMEOUT;
+
+public class TargetTimeoutException extends HandlerException {
+    public TargetTimeoutException(Object uri, Throwable cause) {
+        super("Timed out connecting to " + String.valueOf(uri), cause);
+    }
+
+    @Override
+    public int getHttpStatus() {
+        return HTTP_GATEWAY_TIMEOUT;
+    }
+
+}
