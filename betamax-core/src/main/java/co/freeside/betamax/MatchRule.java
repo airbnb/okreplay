c3a230a8ce9b8fce605d5a54ae97114e9889864f
Sean Freitag <freitag.sean@gmail.com>
2013-08-18 23:54:33 -0500

    Moving all Java files into the Java src folder.

--- betamax-core/src/main/java/co/freeside/betamax/MatchRule.java
+++ betamax-core/src/main/java/co/freeside/betamax/MatchRule.java
@@ -66,7 +66,7 @@ public enum MatchRule implements Comparator<Request> {
             stringObjectTreeMap.putAll(a.getHeaders());
 
             SortedMapDifference<String, Object> difference = Maps.difference(stringObjectTreeMap, b.getHeaders());
-            
+
             return difference.areEqual() ? 0 : -1;
         }
 
