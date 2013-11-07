803b8443f2bc3b0bc48f217a6c4f71b819e85906
George McIntosh <george@georgemcintosh.com>
2013-11-07 19:14:34 +0000

    Removed constructor to allow compilation on Java 1.6

--- betamax-core/src/main/java/co/freeside/betamax/tape/TapeReadException.java
+++ betamax-core/src/main/java/co/freeside/betamax/tape/TapeReadException.java
@@ -33,7 +33,4 @@ public class TapeReadException extends RuntimeException {
         super(cause);
     }
 
-    protected TapeReadException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
