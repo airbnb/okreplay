fd8893f408094835fda680c902b930a6914c71fa
David Kowis <david.kowis@rackspace.com>
2013-11-19 11:09:41 -0600

    Cleaning up imports

--- betamax-proxy/src/test/groovy/co/freeside/betamax/proxy/matchRules/CustomMatcherSpec.groovy
+++ betamax-proxy/src/test/groovy/co/freeside/betamax/proxy/matchRules/CustomMatcherSpec.groovy
@@ -1,28 +1,21 @@
 package co.freeside.betamax.proxy.matchRules
 
-import co.freeside.betamax.MatchRule
 import co.freeside.betamax.ProxyConfiguration
 import co.freeside.betamax.Recorder
 import co.freeside.betamax.TapeMode
-import co.freeside.betamax.message.Request
 import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Unroll
 
 import javax.net.ssl.HttpsURLConnection
-import java.util.concurrent.atomic.AtomicInteger
 
 /**
- * Created by dkowis on 11/14/13.
+ * Testing a custom matcher when being used in the proxy.
  */
 @Unroll
 class CustomMatcherSpec extends Specification {
     @Shared def tapeRoot = new File("src/test/resources/betamax/tapes")
 
-    /**
-     * I can't run both of these tests, and I don't understand why.
-     * I would expect that when using recorder.stop() it'd
-     */
     void "Using a custom matcher, can replay the tape"() {
         given:
         def imr = new InstrumentedMatchRule()
