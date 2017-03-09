ed369db8180973010f598f7ab875f484295a8fa6
Felipe Lima <felipe.lima@gmail.com>
2017-03-09 12:18:28 -0800

    Drop UIAutomator dependency (#11)
    * Remove dependency on uiautomator
    
    * Downgrade minsdkversion

--- walkman-espresso/src/main/kotlin/walkman/PermissionRule.kt
+++ walkman-espresso/src/main/kotlin/walkman/PermissionRule.kt
@@ -1,21 +1,18 @@
 package walkman
 
-import android.support.test.rule.ActivityTestRule
-
 import org.junit.rules.TestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
 
 class PermissionRule(
-    private val configuration: WalkmanConfig,
-    private val activityTestRule: ActivityTestRule<*>) : TestRule {
+    private val configuration: WalkmanConfig) : TestRule {
 
   override fun apply(statement: Statement, description: Description): Statement {
     return object : Statement() {
       @Throws(Throwable::class) override fun evaluate() {
         val tapeRoot = configuration.tapeRoot
         if (tapeRoot is AndroidTapeRoot) {
-          tapeRoot.grantPermissionsIfNeeded(activityTestRule.activity)
+          tapeRoot.grantPermissionsIfNeeded()
         } else {
           throw IllegalArgumentException("TapeRoot needs to be an instance of AndroidTapeRoot")
         }
