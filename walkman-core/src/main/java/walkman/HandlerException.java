a3b69d89eb168a97d7805c465aa4f298c6b93ec5
Felipe Lima <felipe.lima@gmail.com>
2017-04-26 11:51:00 -0700

    Better errors than \"Tape is not writable\"

--- walkman-core/src/main/java/walkman/HandlerException.java
+++ walkman-core/src/main/java/walkman/HandlerException.java
@@ -1,8 +1,6 @@
 package walkman;
 
-/**
- * Thrown to indicates an exception with some part of the handling chain.
- */
+/** Thrown to indicates an exception with some part of the handling chain. */
 abstract class HandlerException extends RuntimeException {
   HandlerException(String message) {
     super(message);
