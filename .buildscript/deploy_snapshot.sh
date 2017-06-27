f97b39f4629fb736b571abd56e79fa1097105483
Felipe Lima <felipe.lima@gmail.com>
2017-06-27 09:32:56 -0700

    Use correct capitalization

--- .buildscript/deploy_snapshot.sh
+++ .buildscript/deploy_snapshot.sh
@@ -6,7 +6,7 @@
 # Adapted from https://coderwall.com/p/9b_lfq and
 # http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
 
-SLUG="airbnb/OkReplay"
+SLUG="airbnb/okreplay"
 JDK="oraclejdk8"
 BRANCH="master"
 
