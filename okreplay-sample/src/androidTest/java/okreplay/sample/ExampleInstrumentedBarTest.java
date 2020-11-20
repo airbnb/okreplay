db1e779030a1d4aad246831ef374a109d69b8b1a
raditya gumay <gumay.raditya@gmail.com>
2020-11-20 16:36:08 +0700

    change import for InstrumentationRegistry

--- okreplay-sample/src/androidTest/java/okreplay/sample/ExampleInstrumentedBarTest.java
+++ okreplay-sample/src/androidTest/java/okreplay/sample/ExampleInstrumentedBarTest.java
@@ -1,11 +1,5 @@
 package okreplay.sample;
 
-import androidx.test.core.app.ApplicationProvider;
-import androidx.test.espresso.IdlingRegistry;
-import androidx.test.espresso.IdlingResource;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.ActivityTestRule;
-
 import com.jakewharton.espresso.OkHttp3IdlingResource;
 
 import org.junit.After;
@@ -15,6 +9,11 @@ import org.junit.Test;
 import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.IdlingResource;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
 import okreplay.AndroidTapeRoot;
 import okreplay.MatchRules;
 import okreplay.OkReplay;
@@ -31,33 +30,36 @@ import static org.hamcrest.CoreMatchers.containsString;
 
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedBarTest {
-  private final DependencyGraph graph = DependencyGraph.Companion.instance();
-  private final ActivityTestRule<MainActivity> activityTestRule =
-      new ActivityTestRule<>(MainActivity.class);
-  private final OkReplayConfig configuration = new OkReplayConfig.Builder()
-      .tapeRoot(new AndroidTapeRoot(InstrumentationRegistry.getContext(), getClass()))
-      .defaultMode(TapeMode.READ_WRITE)
-      .sslEnabled(true)
-      .interceptor(graph.getOkReplayInterceptor())
-      .defaultMatchRules(MatchRules.host, MatchRules.path, MatchRules.method)
-      .build();
-  @Rule public final TestRule testRule =
-      new OkReplayRuleChain(configuration, activityTestRule).get();
-  private final IdlingResource okHttp3IdlingResource =
-      OkHttp3IdlingResource.create("OkHttp", graph.getOkHttpClient());
+	private final DependencyGraph graph = DependencyGraph.Companion.instance();
+	private final ActivityTestRule<MainActivity> activityTestRule =
+		new ActivityTestRule<>(MainActivity.class);
+	private final OkReplayConfig configuration = new OkReplayConfig.Builder()
+		.tapeRoot(new AndroidTapeRoot(InstrumentationRegistry.getInstrumentation().getTargetContext(), getClass()))
+		.defaultMode(TapeMode.READ_WRITE)
+		.sslEnabled(true)
+		.interceptor(graph.getOkReplayInterceptor())
+		.defaultMatchRules(MatchRules.host, MatchRules.path, MatchRules.method)
+		.build();
+	@Rule
+	public final TestRule testRule =
+		new OkReplayRuleChain(configuration, activityTestRule).get();
+	private final IdlingResource okHttp3IdlingResource =
+		OkHttp3IdlingResource.create("OkHttp", graph.getOkHttpClient());
 
-  @Before public void setUp() {
-    IdlingRegistry.getInstance().register(okHttp3IdlingResource);
-  }
+	@Before
+	public void setUp() {
+		IdlingRegistry.getInstance().register(okHttp3IdlingResource);
+	}
 
-  @After public void tearDown() {
-    IdlingRegistry.getInstance().register(okHttp3IdlingResource);
-  }
+	@After
+	public void tearDown() {
+		IdlingRegistry.getInstance().register(okHttp3IdlingResource);
+	}
 
-  @Test
-  @OkReplay
-  public void bar() {
-    onView(withId(R.id.navigation_repositories)).perform(click());
-    onView(withId(R.id.message)).check(matches(withText(containsString("AbsListViewHelper"))));
-  }
+	@Test
+	@OkReplay
+	public void bar() {
+		onView(withId(R.id.navigation_repositories)).perform(click());
+		onView(withId(R.id.message)).check(matches(withText(containsString("AbsListViewHelper"))));
+	}
 }
