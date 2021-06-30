c44efafd59f9e255ecbd6b24dbd5ef4d570d4349
raditya gumay <gumay.raditya@gmail.com>
2021-06-30 23:52:29 +0800

    [Fix] bump version vanniktech to 0.11.1 (#107)
    * [Fix] bump version vanniktech to 0.11.1
    
    * change import for InstrumentationRegistry

--- okreplay-sample/src/androidTest/java/okreplay/sample/ExampleInstrumentedFooTest.java
+++ okreplay-sample/src/androidTest/java/okreplay/sample/ExampleInstrumentedFooTest.java
@@ -1,11 +1,5 @@
 package okreplay.sample;
 
-import androidx.test.core.app.ApplicationProvider;
-import androidx.test.espresso.IdlingRegistry;
-import androidx.test.espresso.IdlingResource;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.ActivityTestRule;
-
 import com.jakewharton.espresso.OkHttp3IdlingResource;
 
 import org.junit.After;
@@ -15,6 +9,12 @@ import org.junit.Test;
 import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.IdlingResource;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
 import okreplay.AndroidTapeRoot;
 import okreplay.AssetManager;
 import okreplay.MatchRules;
@@ -33,36 +33,39 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedFooTest {
-  private final DependencyGraph graph = DependencyGraph.Companion.instance();
-  private final ActivityTestRule<MainActivity> activityTestRule =
-      new ActivityTestRule<>(MainActivity.class);
-  private final AssetManager assetManager =
-      new AssetManager(InstrumentationRegistry.getContext());
-  private final OkReplayConfig configuration = new OkReplayConfig.Builder()
-      .tapeRoot(new AndroidTapeRoot(assetManager, getClass().getSimpleName()))
-      .defaultMode(TapeMode.READ_WRITE)
-      .sslEnabled(true)
-      .interceptor(graph.getOkReplayInterceptor())
-      .defaultMatchRules(MatchRules.host, MatchRules.path, MatchRules.method)
-      .build();
-  @Rule public final TestRule testRule =
-      new OkReplayRuleChain(configuration, activityTestRule).get();
-  private final IdlingResource okHttp3IdlingResource =
-      OkHttp3IdlingResource.create("OkHttp", graph.getOkHttpClient());
+	private final DependencyGraph graph = DependencyGraph.Companion.instance();
+	private final ActivityTestRule<MainActivity> activityTestRule =
+		new ActivityTestRule<>(MainActivity.class);
+	private final AssetManager assetManager =
+		new AssetManager(InstrumentationRegistry.getInstrumentation().getTargetContext());
+	private final OkReplayConfig configuration = new OkReplayConfig.Builder()
+		.tapeRoot(new AndroidTapeRoot(assetManager, getClass().getSimpleName()))
+		.defaultMode(TapeMode.READ_WRITE)
+		.sslEnabled(true)
+		.interceptor(graph.getOkReplayInterceptor())
+		.defaultMatchRules(MatchRules.host, MatchRules.path, MatchRules.method)
+		.build();
+	@Rule
+	public final TestRule testRule =
+		new OkReplayRuleChain(configuration, activityTestRule).get();
+	private final IdlingResource okHttp3IdlingResource =
+		OkHttp3IdlingResource.create("OkHttp", graph.getOkHttpClient());
 
-  @Before public void setUp() {
-    IdlingRegistry.getInstance().register(okHttp3IdlingResource);
-  }
+	@Before
+	public void setUp() {
+		IdlingRegistry.getInstance().register(okHttp3IdlingResource);
+	}
 
-  @After public void tearDown() {
-    IdlingRegistry.getInstance().register(okHttp3IdlingResource);
-  }
+	@After
+	public void tearDown() {
+		IdlingRegistry.getInstance().register(okHttp3IdlingResource);
+	}
 
-  @Test
-  @OkReplay
-  public void foo() {
-    assertEquals("okreplay.sample", ApplicationProvider.getApplicationContext().getPackageName());
-    onView(withId(R.id.navigation_repositories)).perform(click());
-    onView(withId(R.id.message)).check(matches(withText(containsString("6502Android"))));
-  }
+	@Test
+	@OkReplay
+	public void foo() {
+		assertEquals("okreplay.sample", ApplicationProvider.getApplicationContext().getPackageName());
+		onView(withId(R.id.navigation_repositories)).perform(click());
+		onView(withId(R.id.message)).check(matches(withText(containsString("6502Android"))));
+	}
 }
